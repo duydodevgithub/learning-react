611c19099e387e57ad29acd65a8d94f805b2da1c
DuyDo <duydo.dev@gmail.com>
2018-07-31 21:27:29 -0500

    ..

--- udacity_react/main.js
+++ udacity_react/main.js
@@ -1,31 +1,31 @@
-// function createStore() {
-//     // The store function should have four parts
-//     //1. The state
-//     //2. Get the state
-//     //3. Listen to change on the state
-//     //4. Update the state
-//     let state;
-//     let listeners = [];
+function createStore() {
+    // The store function should have four parts
+    //1. The state
+    //2. Get the state
+    //3. Listen to change on the state
+    //4. Update the state
+    let state;
+    let listeners = [];
 
-//     const getState = () => state;
-//     const subscribe = (listener) => {
-//         listeners.push(listener);
-//         return () => {
-//             listeners = listeners.filter((l) => l !== listener);
-//         }
-//     }
-//     return {
-//         getState,
-//         subscribe
-//     }
+    const getState = () => state;
+    const subscribe = (listener) => {
+        listeners.push(listener);
+        return () => {
+            listeners = listeners.filter((l) => l !== listener);
+        }
+    }
+    return {
+        getState,
+        subscribe
+    }
 
-// }
+}
 
-// const store = createStore();
-// store.subscribe(() => {
-//     console.log("The new state is: ", store.getState());
-// })
+const store = createStore();
+store.subscribe(() => {
+    console.log("The new state is: ", store.getState());
+})
 
-// const unsubscribe = store.subscribe(() => {
-//     console.log("The store changed.");
-// })
+const unsubscribe = store.subscribe(() => {
+    console.log("The store changed.");
+})
