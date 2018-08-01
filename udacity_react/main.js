37ca105f6c9e151133dd5e73e6d5d2a8f1894107
DuyDo <duydo.dev@gmail.com>
2018-07-31 23:56:34 -0500

    add reducer

--- udacity_react/main.js
+++ udacity_react/main.js
@@ -1,4 +1,6 @@
-function createStore() {
+//library code
+
+function createStore(reducer) {
     // The store function should have four parts
     //1. The state
     //2. Get the state
@@ -14,17 +16,37 @@ function createStore() {
             listeners = listeners.filter((l) => l !== listener);
         }
     }
+
+    const dispatch = (action) => {
+        state = reducer(state, action);
+        listeners.forEach((listener) => listener());
+    }
+
     return {
         getState,
-        subscribe
+        subscribe,
+        dispatch
     }
 
 }
 
+//app code
+
+function todos(state = [], action) {
+    if(action.type === "ADD_TODO") {
+        return state.concat([action.todo]);
+    }
+
+    return state;
+}
+
+
 const store = createStore();
+console.log(store);
 store.subscribe(() => {
     console.log("The new state is: ", store.getState());
 })
+
 
 const unsubscribe = store.subscribe(() => {
     console.log("The store changed.");
